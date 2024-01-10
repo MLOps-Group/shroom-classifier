c8b05d0397adae0a44135c359ff05ebac6a98661
Peter Grønning <s183922@student.dtu.dk>
2024-01-10 15:39:53 +0100

    started with configs....

--- shroom_classifier/train_model.py
+++ shroom_classifier/train_model.py
@@ -4,18 +4,22 @@ from torch.utils.data import DataLoader
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger
 
+def train():
+    model = ShroomClassifierMobileNetV3Large100(N_SUPER_CLASSES)
+    preprocesser = model.preprocesser
 
-model = ShroomClassifierMobileNetV3Large100(N_SUPER_CLASSES)
-preprocesser = model.preprocesser
+    train_dataset = ShroomDataset("train", datapath="data/raw", preprocesser=preprocesser)
+    train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=8)
 
-train_dataset = ShroomDataset("train", datapath="data/raw", preprocesser=preprocesser)
-train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=8)
+    val_dataset = ShroomDataset("val", datapath="data/raw", preprocesser=preprocesser)
+    val_datalaoder = DataLoader(val_dataset, batch_size=64, shuffle=True, num_workers=8)
 
-val_dataset = ShroomDataset("val", datapath="data/raw", preprocesser=preprocesser)
-val_datalaoder = DataLoader(val_dataset, batch_size=64, shuffle=True, num_workers=8)
+    model.train()
+    wandb_logger = WandbLogger()
+    trainer = Trainer(max_epochs=10, logger=wandb_logger)
+    trainer.fit(model, train_dataloader)
 
-model.train()
-wandb_logger = WandbLogger()
-trainer = Trainer(max_epochs=10, logger=wandb_logger)
-trainer.fit(model, train_dataloader)
 
+
+if __name__ == "__main__":
+    train()
