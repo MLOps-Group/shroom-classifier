7723f4bf8f4a4c0e53329db2fbc998f421d25e98
Peter Grønning <s183922@student.dtu.dk>
2024-01-11 11:51:10 +0100

    metrics

--- shroom_classifier/models/model.py
+++ shroom_classifier/models/model.py
@@ -5,13 +5,13 @@ from pytorch_lightning import LightningModule
 from shroom_classifier.data.dataset import ShroomDataset
 from timm.data import resolve_data_config
 from timm.data.transforms_factory import create_transform
-from timm.loss import BinaryCrossEntropy
+from timm.loss import BinaryCrossEntropy 
 from torch import optim
 import torch
 import matplotlib.pyplot as plt
 import wandb
 import numpy as np
-
+from sklearn.metrics import precision_recall_fscore_support, accuracy_score
 
 class ShroomClassifierMobileNetV3Large100(LightningModule):
     def __init__(self, num_classes: int):
@@ -30,34 +30,68 @@ class ShroomClassifierMobileNetV3Large100(LightningModule):
 
 
         if self.global_step % 10 == 0:
-            self.logger.experiment.log({"train_loss": loss})
+            self.logger.experiment.log({"train/loss": loss})
             self.logger.experiment.log({"trainer/step": self.global_step})
-            logits = y_hat[0]
-            probs = torch.exp(logits) / torch.exp(logits).sum()
-            probs = probs.detach().cpu().numpy()    
-            prediction = torch.argmax(y_hat.sigmoid(), axis=0).detach().cpu().numpy()
-            targets    = torch.argmax(super_classes, axis=0).detach().cpu().numpy()
 
-            acc = np.mean(prediction == targets)
-            self.logger.experiment.log({"train_acc": acc})
-            fig, ax = plt.subplots(figsize=(10, 10))
-            ax.bar(range(len(probs)), probs * 3)
-            y_true = super_classes[0].detach().cpu().numpy()
-            ax.bar(range(len(probs)), y_true, alpha=0.3, color = "green", width = 5)
+            probs = torch.nn.functional.softmax(y_hat, dim=1).detach().cpu().numpy()
+            prediction = probs.argmax(axis=1)
+            targets    = super_classes.detach().cpu().numpy()
+            acc, precision, recall, f1, support = get_metrics(targets.argmax(axis = 1), prediction)
+            self.logger.experiment.log({"train/acc": acc,
+                                        "train/precision": precision,
+                                        "train/recall": recall,
+                                        "train/f1": f1,
+                                        "train/support": support})
 
-            
-            image = wandb.Image(fig)
+            image = plot_probs(targets[0], probs[0])
             self.logger.experiment.log({"probs": image})
-            plt.close()
-
 
         return loss
-    
 
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
     
 
+def get_metrics(y_true: np.ndarray, y_hat: np.ndarray) -> (float, float, float, float, float):
+    ''' Computes classification metrics:
+         - Accuracy
+        - Precision
+        - Recall
+        - F1-score
+        - Support
 
+        Args:
+            y_true: True labels (N_CLASSES)
+            y_hat: Predicted labels (N_CLASSES)
+
+        Returns:
+            accuracy: Accuracy score
+            precision: Precision score
+            recall: Recall score
+            f1: F1 score
+    '''
     
+    accuracy = accuracy_score(y_true, y_hat)
+    precision, recall, f1, support = precision_recall_fscore_support(y_true, y_hat, average="macro")
+
+    return accuracy, precision, recall, f1, support
+
+
+def plot_probs(y_true: np.ndarray, probs: np.ndarray) -> wandb.Image:
+    ''' Plots the probabilities of the predicted classes.
+
+        Args:
+            y_true: True labels (BATCH_SIZE, N_CLASSES)
+            probs: Predicted probabilities (BATCH_SIZE, N_CLASSES)
+
+        Returns:
+            fig: Figure containing the plot
+    '''
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.bar(range(len(probs)), probs, label = "Predicted probabilities")
+    ax.bar(range(len(probs)), y_true, alpha=0.3, color = "green", width = 5, label = "True label")
+    ax.grid(linestyle='--', linewidth=1, axis='y')
+    img = wandb.Image(fig)
+    plt.close()
+    return img
