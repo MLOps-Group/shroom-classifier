5f0066b3bfe270fa577f0b19e5735073807aff3b
Lukas Leindals <s183920@student.dtu.dk>
2024-01-11 11:18:13 +0100

    create image_to_tensor util

---
+++ shroom_classifier/data/utils.py
@@ -0,0 +1,14 @@
+from PIL import Image
+import numpy as np
+import torch
+
+def image_to_tensor(image_path: str, preprocesser = None):
+    img = Image.open(image_path)
+    
+    if preprocesser is not None:
+        img = preprocesser(img)
+    else:
+        img = np.array(img)
+        img = torch.tensor(img).permute(2, 0, 1).float()
+        
+    return img
