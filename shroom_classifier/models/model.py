eeb728bf0be8276dfe7dbf9e08ddce839b10b4ee
Peter Grønning <s183922@student.dtu.dk>
2024-01-12 11:03:21 +0100

    wandb logging

--- shroom_classifier/models/model.py
+++ shroom_classifier/models/model.py
@@ -18,33 +18,38 @@ class ShroomClassifierResNet(LightningModule):
         super().__init__()
         self.model = timm.create_model('resnet50.a1_in1k', pretrained=True, num_classes=num_classes)
         self.preprocesser = create_transform(**resolve_data_config(self.model.pretrained_cfg))
-        self.loss = torch.nn.BCEWithLogitsLoss()
+        self.loss = BinaryCrossEntropy()
 
     def forward(self, x):
         return self.model(x)
+       
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1e-3)                      # TODO: Change with config
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)   # TODO: Change with config
+
+        return {'optimizer': optimizer, 'lr_scheduler': scheduler, "monitor": "val/loss"}
     
+
     def training_step(self, batch, batch_idx):
         images, classes, super_classes = batch
         y_hat = self(images)
         loss = self.loss(y_hat, super_classes)
 
         # Log metrics
-        if self.global_step % 10 == 0:
+        if self.global_step % 20 == 0:
             self.logger.experiment.log({"train/loss": loss})
             self.logger.experiment.log({"trainer/global_step": self.global_step})
 
             probs = torch.nn.functional.softmax(y_hat, dim=1).detach().cpu().numpy()
             prediction = probs.argmax(axis=1)
             targets    = super_classes.detach().cpu().numpy()
-            acc, precision, recall, f1, support = get_metrics(targets.argmax(axis = 1), prediction)
-            self.logger.experiment.log({"train/acc": acc,
+            acc, precision, recall, f1 = get_metrics(targets.argmax(axis = 1), prediction)
+            self.logger.experiment.log({"train/accuracy": acc,
                                         "train/precision": precision,
                                         "train/recall": recall,
-                                        "train/f1": f1,
-                                        "train/support": support})
+                                        "train/f1": f1})
 
-            image = plot_probs(targets[0], probs[0])
-            self.logger.experiment.log({"probs": image})
+
 
         return loss
 
@@ -53,26 +58,22 @@ class ShroomClassifierResNet(LightningModule):
         y_hat = self(images)
         loss = self.loss(y_hat, super_classes)
 
-        # Log metrics
-        # if self.global_step % 10 == 0:
-        self.logger.experiment.log({"val/loss": loss})
-        self.logger.experiment.log({"trainer/step": self.global_step})
-
         probs = torch.nn.functional.softmax(y_hat, dim=1).detach().cpu().numpy()
         prediction = probs.argmax(axis=1)
         targets    = super_classes.detach().cpu().numpy()
-        acc, precision, recall, f1, support = get_metrics(targets.argmax(axis = 1), prediction)
-        self.logger.experiment.log({"val/acc": acc,
-                                    "val/precision": precision,
-                                    "val/recall": recall,
-                                    "val/f1": f1,
-                                    "val/support": support})
+        acc, precision, recall, f1 = get_metrics(targets.argmax(axis = 1), prediction)
+
+        self.log("val/loss", loss)
+        self.log("val/accuracy", acc)
+        self.log("val/precision", precision)
+        self.log("val/recall", recall)
+        self.log("val/f1", f1)
+
+
         return loss
     
-    def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=1e-3)
-        return optimizer
-    
+
+
 
 def get_metrics(y_true: np.ndarray, y_hat: np.ndarray) -> (float, float, float, float, float):
     ''' Computes classification metrics:
@@ -94,25 +95,6 @@ def get_metrics(y_true: np.ndarray, y_hat: np.ndarray) -> (float, float, float, 
     '''
     
     accuracy = accuracy_score(y_true, y_hat)
-    precision, recall, f1, support = precision_recall_fscore_support(y_true, y_hat, average="macro", zero_division=0)
+    precision, recall, f1, _ = precision_recall_fscore_support(y_true, y_hat, average="macro", zero_division=0)
 
-    return accuracy, precision, recall, f1, support
-
-
-def plot_probs(y_true: np.ndarray, probs: np.ndarray) -> wandb.Image:
-    ''' Plots the probabilities of the predicted classes.
-
-        Args:
-            y_true: True labels (BATCH_SIZE, N_CLASSES)
-            probs: Predicted probabilities (BATCH_SIZE, N_CLASSES)
-
-        Returns:
-            fig: Figure containing the plot
-    '''
-    fig, ax = plt.subplots(figsize=(10, 10))
-    ax.bar(range(len(probs)), probs, label = "Predicted probabilities")
-    ax.bar(range(len(probs)), y_true, alpha=0.3, color = "green", width = 5, label = "True label")
-    ax.grid(linestyle='--', linewidth=1, axis='y')
-    img = wandb.Image(fig)
-    plt.close()
-    return img
+    return accuracy, precision, recall, f1
