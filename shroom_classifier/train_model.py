f30c11d77cdb2b5ffb78d806de33332ea58443e5
Peter Grønning <s183922@student.dtu.dk>
2024-01-12 19:52:28 +0100

    Hydra and stuff

--- shroom_classifier/train_model.py
+++ shroom_classifier/train_model.py
@@ -6,7 +6,8 @@ from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.callbacks import Callback, LearningRateMonitor
 import wandb
-
+import torch
+import hydra
 
 WANDB_ENTITY = "mlops_papersummarizer"      # Wandb entity name
 PROJECT_NAME = "dev"                        # Wandb project name
@@ -15,27 +16,31 @@ MONITOR = 'accuracy'                        # Validation metric to monitor (save
 MODEL_DIR = 'models/'                       # Directory to save the model
 
 
-
-def train():
-    model = ShroomClassifierResNet(N_SUPER_CLASSES)
-    preprocesser = model.preprocesser
-
-    train_dataset = ShroomDataset("sample", datapath="data/processed/", preprocesser=preprocesser)
-    # train_dataset = torch.utils.data.Subset(train_dataset, range(0, 100))
-    train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=8)
-
-    val_dataset = ShroomDataset("sample", datapath="data/processed", preprocesser=preprocesser) # Train = Val (for now)
-    val_datalaoder = DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=8)
-
-    wandb_logger = WandbLogger(entity = WANDB_ENTITY, project = PROJECT_NAME, mode = MODE, log_model = "all")
-
-    montior_name = f"val/{MONITOR}"
-    checkpoint_callback = ModelCheckpoint(monitor=f"val/{MONITOR}", save_top_k=1, mode='max', dirpath=MODEL_DIR, filename='epoch={epoch}_val_acc={val/accuracy:.2f}', auto_insert_metric_name = False)
-    lr_monitor = LearningRateMonitor(logging_interval='step')
+@hydra.main(config_path="../configs", config_name="config", version_base = None)
+def train(cfg):
+    print(cfg.experiments)
+    hparams = cfg.experiment.hparams
+    wandb_params = cfg.experiment.wandb_params
+    model = ShroomClassifierResNet(hparams)
 
 
-    trainer = Trainer(max_epochs=10, logger=wandb_logger, callbacks=[checkpoint_callback, lr_monitor])
-    trainer.fit(model, train_dataloader, val_datalaoder)
+    torch.manual_seed(hparams.seed)
+    train_dataset = ShroomDataset(**cfg.train_data, preprocesser=model.preprocesser)
+    train_dataloader = DataLoader(train_dataset, batch_size=hparams.batch_size, shuffle=True, num_workers=cfg.data.num_workers, seed=hparams.seed)
+
+    val_dataset = ShroomDataset(**cfg.val_data, preprocesser=model.preprocesser) # Train = Val (for now)
+    val_datalaoder = DataLoader(val_dataset, batch_size=hparams.batch_size, shuffle=False, num_workers=cfg.data.num_workers)
+
+
+    wandb_logger = WandbLogger(**wandb_params)
+
+    # montior_name = f"val/{MONITOR}"
+    # checkpoint_callback = ModelCheckpoint(monitor=f"val/{MONITOR}", save_top_k=1, mode='max', dirpath=MODEL_DIR, filename='epoch={epoch}_val_acc={val/accuracy:.2f}', auto_insert_metric_name = False)
+    # lr_monitor = LearningRateMonitor(logging_interval='step')
+
+
+    # trainer = Trainer(max_epochs=10, logger=wandb_logger, callbacks=[checkpoint_callback, lr_monitor])
+    # trainer.fit(model, train_dataloader, val_datalaoder)
 
 
 
