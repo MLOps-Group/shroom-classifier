cdd603727ec2ce0b21784f6f68c42bdeacf016f8
Peter Grønning <s183922@student.dtu.dk>
2024-01-12 11:11:34 +0100

    Merge changes with maset

--- shroom_classifier/models/model.py
+++ shroom_classifier/models/model.py
@@ -11,6 +11,7 @@ import torch
 from shroom_classifier.visualization.train_plots import plot_probs
 from shroom_classifier.evaluation.metrics import get_metrics
 
+
 class ShroomClassifierResNet(LightningModule):
     def __init__(self, num_classes: int):
         super().__init__()
@@ -49,8 +50,6 @@ class ShroomClassifierResNet(LightningModule):
                                         "train/recall": recall,
                                         "train/f1": f1})
 
-
-
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -73,28 +72,3 @@ class ShroomClassifierResNet(LightningModule):
         return loss
     
 
-
-
-def get_metrics(y_true: np.ndarray, y_hat: np.ndarray) -> (float, float, float, float, float):
-    ''' Computes classification metrics:
-         - Accuracy
-        - Precision
-        - Recall
-        - F1-score
-        - Support
-
-        Args:
-            y_true: True labels (N_CLASSES)
-            y_hat: Predicted labels (N_CLASSES)
-
-        Returns:
-            accuracy: Accuracy score
-            precision: Precision score
-            recall: Recall score
-            f1: F1 score
-    '''
-    
-    accuracy = accuracy_score(y_true, y_hat)
-    precision, recall, f1, _ = precision_recall_fscore_support(y_true, y_hat, average="macro", zero_division=0)
-
-    return accuracy, precision, recall, f1
