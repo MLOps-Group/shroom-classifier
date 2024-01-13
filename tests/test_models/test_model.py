a67d9b58e8b0547eb8d2abe963399d798414e70a
Lukas Leindals <s183920@student.dtu.dk>
2024-01-13 15:51:34 +0100

    fix hydra usage

--- tests/test_models/test_model.py
+++ tests/test_models/test_model.py
@@ -1,9 +1,16 @@
 from shroom_classifier.models import load_model
 import hydra
 
-@hydra.main(config_path="../../configs/", config_name="test_values.yaml")
-def test_model(cfg):
+
+def test_model():
+    # create a config object
+    with hydra.initialize(version_base=None, config_path="../../configs/"):
+        cfg = hydra.compose(config_name="test_values.yaml")#, overrides=["app.user=test_user"])
+    
+    # load the model
     model, _ = load_model("wandb:mlops_papersummarizer/dev/model-dct9b3c3:v3")
+    
+    # check model hyperparameters
     assert model.hparams["num_classes"] == cfg.model.num_classes
     
 if __name__ == "__main__":
