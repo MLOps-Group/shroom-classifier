59dcdc0add85de86f28789667181721c1a61a0a8
Lukas Leindals <s183920@student.dtu.dk>
2024-01-13 16:54:52 +0100

    add train config

--- shroom_classifier/train_model.py
+++ shroom_classifier/train_model.py
@@ -9,38 +9,41 @@ import wandb
 import torch
 import hydra
 
-WANDB_ENTITY = "mlops_papersummarizer"      # Wandb entity name
-PROJECT_NAME = "dev"                        # Wandb project name
-MODE = 'online'                             # Wandb mode (online/offline)
-MONITOR = 'accuracy'                        # Validation metric to monitor (saves the best performing model)
-MODEL_DIR = 'models/'                       # Directory to save the model
+# WANDB_ENTITY = "mlops_papersummarizer"      # Wandb entity name
+# PROJECT_NAME = "dev"                        # Wandb project name
+# MODE = 'online'                             # Wandb mode (online/offline)
+# MONITOR = 'accuracy'                        # Validation metric to monitor (saves the best performing model)
+# MODEL_DIR = 'models/'                       # Directory to save the model
 
 
 @hydra.main(config_path="../configs", config_name="config", version_base = None)
 def train(cfg):
-    print(cfg.experiments)
-    hparams = cfg.experiment.hparams
-    wandb_params = cfg.experiment.wandb_params
-    model = ShroomClassifierResNet(hparams)
+    # extract train config
+    config = cfg.train
 
+    # init model
+    model = ShroomClassifierResNet(**config.model)
+    
+    # set seed
+    torch.manual_seed(config.seed)
 
-    torch.manual_seed(hparams.seed)
-    train_dataset = ShroomDataset(**cfg.train_data, preprocesser=model.preprocesser)
-    train_dataloader = DataLoader(train_dataset, batch_size=hparams.batch_size, shuffle=True, num_workers=cfg.data.num_workers, seed=hparams.seed)
+    # create train dataloader
+    train_dataset = ShroomDataset(**config.train_dataset, preprocesser=model.preprocesser)
+    train_dataloader = DataLoader(train_dataset, **config.train_dataloader)
 
-    val_dataset = ShroomDataset(**cfg.val_data, preprocesser=model.preprocesser) # Train = Val (for now)
-    val_datalaoder = DataLoader(val_dataset, batch_size=hparams.batch_size, shuffle=False, num_workers=cfg.data.num_workers)
+    # create val dataloader
+    val_dataset = ShroomDataset(**config.val_dataset, preprocesser=model.preprocesser) # Train = Val (for now)
+    val_dataloader = DataLoader(val_dataset, **config.val_dataloader)   
 
+    # init callbacks
+    checkpoint_callback = ModelCheckpoint(**config.checkpoint_callback)
+    lr_monitor = LearningRateMonitor(**config.lr_monitor)
 
-    wandb_logger = WandbLogger(**wandb_params)
+    # init wandb
+    wandb_logger = WandbLogger(**config.wandb, config=dict(config))
 
-    # montior_name = f"val/{MONITOR}"
-    # checkpoint_callback = ModelCheckpoint(monitor=f"val/{MONITOR}", save_top_k=1, mode='max', dirpath=MODEL_DIR, filename='epoch={epoch}_val_acc={val/accuracy:.2f}', auto_insert_metric_name = False)
-    # lr_monitor = LearningRateMonitor(logging_interval='step')
-
-
-    # trainer = Trainer(max_epochs=10, logger=wandb_logger, callbacks=[checkpoint_callback, lr_monitor])
-    # trainer.fit(model, train_dataloader, val_datalaoder)
+    trainer = Trainer(**config.trainer, logger=wandb_logger, callbacks=[checkpoint_callback, lr_monitor])
+    trainer.fit(model, train_dataloader, val_dataloader)
 
 
 
