3d14d2732284d214cd667f42621fd9089273fd8b
Gustav Larsen <s180820@student.dtu.dk>
2024-01-14 15:22:53 +0100

    formatting changes plus pytesting updates

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,13 +1,6 @@
-from shroom_classifier.predict_model import ShroomPredictor
-
-def test_predict(): 
-    predictor = ShroomPredictor("wandb:mlops_papersummarizer/dev/model-dct9b3c3:v3")
-    probs = predictor.predict("data/processed/sample/10000_Abortiporus_biennis/FVL2009PIC49049490.JPG")
-    assert probs is not None
-
-def test_top_k_preds():
-    predictor = ShroomPredictor("wandb:mlops_papersummarizer/dev/model-dct9b3c3:v3")
-    top_k_preds = predictor.top_k_preds("data/processed/sample/10000_Abortiporus_biennis/FVL2009PIC49049490.JPG")
-    assert top_k_preds is not None
+from shroom_classifier.models.model import ShroomClassifierResNet
 
 
+def test_model() -> None:
+    model = ShroomClassifierResNet(num_classes=10)
+    assert model is not None
