776e974f6c6c89bdd5560fea844b01d14f03f51c
Lukas Leindals <s183920@student.dtu.dk>
2024-01-15 09:58:20 +0100

    update test to use hydra config

--- tests/test_model.py
+++
@@ -1,6 +0,0 @@
-from shroom_classifier.models.model import ShroomClassifierResNet
-
-
-def test_model() -> None:
-    model = ShroomClassifierResNet(num_classes=10)
-    assert model is not None
--- tests/test_models/test_model.py
+++ tests/test_models/test_model.py
@@ -1,13 +1,24 @@
-# def test_model():
-#     # create a config object
-#     with hydra.initialize(version_base=None, config_path="../../configs/pytest_config"):
-#         cfg = hydra.compose(config_name="test_values.yaml")#, overrides=["app.user=test_user"])
+from shroom_classifier.models import load_model
+import hydra
 
-#     # load the model
-#     model, _ = load_model("wandb:mlops_papersummarizer/dev/model-dct9b3c3:v3")
+def test_model():
+    # create a config object
+    with hydra.initialize(version_base=None, config_path="../../configs/pytest_config"):
+        cfg = hydra.compose(config_name="test_values.yaml")#, overrides=["app.user=test_user"])
 
-#     # check model hyperparameters
-#     assert model.hparams["num_classes"] == cfg.model.num_classes
+    # load the model
+    model, _ = load_model("wandb:mlops_papersummarizer/dev/model-dct9b3c3:v3")
 
-# if __name__ == "__main__":
-#     test_model()
+    # check model hyperparameters
+    assert model.hparams["num_classes"] == cfg.model.num_classes
+
+
+# from shroom_classifier.models.model import ShroomClassifierResNet
+
+
+# def test_model() -> None:
+#     model = ShroomClassifierResNet(num_classes=10)
+#     assert model is not None
+
+if __name__ == "__main__":
+    test_model()
