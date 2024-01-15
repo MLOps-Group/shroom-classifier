84ab76f96dc49bc1b19c22b31f4d1b8fac0be687
Lukas Leindals <s183920@student.dtu.dk>
2024-01-15 10:56:44 +0100

    add config to tet

--- tests/test_data/test_data.py
+++ tests/test_data/test_data.py
@@ -1,16 +1,24 @@
 from shroom_classifier.data.dataset import ShroomDataset
 import os
 import pytest
+from shroom_classifier.utils import get_config
 
 @pytest.mark.skipif(not os.path.exists("data/processed/"), reason="Data files not found")
 def test_data():
-    train_dataset = ShroomDataset("sample", datapath="data/processed/", preprocesser=None)
-    val_dataset = ShroomDataset("sample", datapath="data/processed", preprocesser=None)  # Train = Val (for now)
+    # get config
+    cfg = get_config("test_values.yaml", config_folder="pytest_config")
+    
+    # create datasets
+    train_dataset = ShroomDataset(dataname = cfg.data.train.dataname, datapath=cfg.data.train.datapath, preprocesser=None)
+    val_dataset = ShroomDataset(dataname = cfg.data.val.dataname, datapath=cfg.data.val.datapath, preprocesser=None)  # Train = Val (for now)
 
-    assert len(train_dataset) != 0
-    assert len(val_dataset) != 0
-    #assert train_dataset[0][0].shape == (3, 224, 224)
-    #assert val_dataset[0][0].shape == (3, 224, 224)
+    # check that datasets are not empty
+    assert len(train_dataset) != 0, "Train dataset is empty"
+    assert len(val_dataset) != 0, "Val dataset is empty"
+    
+    # check that datasets have the correct shape
+    assert train_dataset[0][0].shape == (3, 224, 224), f"Train dataset has shape {train_dataset[0][0].shape} instead of (3, 224, 224)"
+    assert val_dataset[0][0].shape == (3, 224, 224), f"Val dataset has shape {val_dataset[0][0].shape} instead of (3, 224, 224)"
 
 
 if __name__ == "__main__":
--- tests/test_predict_model.py
+++ tests/test_predict_model.py
@@ -1,13 +1,19 @@
 from shroom_classifier.predict_model import ShroomPredictor
-
+from shroom_classifier.utils import get_config
 
 def test_predict() -> None:
-    predictor = ShroomPredictor("wandb:mlops_papersummarizer/dev/model-dct9b3c3:v3")
-    probs = predictor.predict("data/processed/sample/10000_Abortiporus_biennis/FVL2009PIC49049490.JPG")
+    # get config
+    cfg = get_config("test_values.yaml", config_folder="pytest_config")
+    
+    predictor = ShroomPredictor(cfg.model.model_path, download_path=cfg.model.download_path)
+    probs = predictor.predict(cfg.predict.test_file)
     assert probs is not None
 
 
 def test_top_k_preds() -> None:
-    predictor = ShroomPredictor("wandb:mlops_papersummarizer/dev/model-dct9b3c3:v3")
-    top_k_preds = predictor.top_k_preds("data/processed/sample/10000_Abortiporus_biennis/FVL2009PIC49049490.JPG")
+    # get config
+    cfg = get_config("test_values.yaml", config_folder="pytest_config")
+    
+    predictor = ShroomPredictor(cfg.model.model_path, download_path=cfg.model.download_path)
+    top_k_preds = predictor.top_k_preds(cfg.predict.test_file, k=cfg.predict.k)
     assert top_k_preds is not None
