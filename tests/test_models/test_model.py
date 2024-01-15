675bda0ce9aa776378a72092231a3afc61a6458d
Lukas Leindals <s183920@student.dtu.dk>
2024-01-15 10:56:44 +0100

    add config to tet

--- tests/test_models/test_model.py
+++ tests/test_models/test_model.py
@@ -1,24 +1,16 @@
 from shroom_classifier.models import load_model
-import hydra
+from shroom_classifier.utils import get_config
 
 def test_model():
     # create a config object
-    with hydra.initialize(version_base=None, config_path="../../configs/pytest_config"):
-        cfg = hydra.compose(config_name="test_values.yaml")#, overrides=["app.user=test_user"])
+    cfg = get_config("test_values.yaml", config_folder="pytest_config")
 
     # load the model
-    model, _ = load_model("wandb:mlops_papersummarizer/dev/model-dct9b3c3:v3")
+    model, _ = load_model(cfg.model.model_path, download_path=cfg.model.download_path)
 
     # check model hyperparameters
     assert model.hparams["num_classes"] == cfg.model.num_classes
 
-
-# from shroom_classifier.models.model import ShroomClassifierResNet
-
-
-# def test_model() -> None:
-#     model = ShroomClassifierResNet(num_classes=10)
-#     assert model is not None
 
 if __name__ == "__main__":
     test_model()
